b33297ee179278bb3920f41291a5b91030007802
Jessica Morgan <jessicamorgan@Jessicas-MacBook-Pro-104.local>
2018-05-17 13:27:28 -0400

    Original

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: 'i9F5Pbp3ITE1aFONt2GKJ5io3',
+  consumer_secret: 'CsCsYkbGFD11Fyp4pXUUFO6jPXjXgJxCpvYSwTW6iOevtm3Fl',
+  access_token_key: '996929879122874368-hjOKWad7eSMplkQdQINWIgWJMcHVfCn',
+  access_token_secret: 'GYTe8VYnBklcrA32DfGQ35cpcSl9F0LRqmn9u0BJefoh2'
+};
+
+exports.spotify = {
+  id: '047a0a6250a845738566dad8e0f546df',
+  secret: '748fd2103dfc486e844d1473073962cb'
+};
